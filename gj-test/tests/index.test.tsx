f9f26af4d0122baec125026c6b9a0d6181a42686
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-05-23 23:21:47 +0100

    remove create-next-app content

--- gj-test/tests/index.test.tsx
+++ gj-test/tests/index.test.tsx
@@ -3,12 +3,5 @@ import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
-    expect(
-      screen.getByText(
-        /Discover and deploy boilerplate example Next.js projects./i
-      )
-    ).toBeInTheDocument();
-  });
+  it("", () => {});
 });
